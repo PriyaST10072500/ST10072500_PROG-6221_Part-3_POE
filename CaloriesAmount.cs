ec425df871683f2cddb4e42f043e837cf756f6a3
priya <ST10072500@vcconnect.edu.za>
2024-06-27 11:28:44 +0200

    Added References

--- CaloriesAmount.cs
+++ CaloriesAmount.cs
@@ -51,4 +51,19 @@ namespace ST10072500_PROG_6221_Part_3_POE
     }
 }
 
+//References 
+//https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/selection-statements
+//https://www.geeksforgeeks.org/c-sharp-how-to-change-foreground-color-of-text-in-console/
+//https://stackoverflow.com/questions/14792066/change-background-color-on-c-sharp-console-application
+//https://www.webstaurantstore.com/guide/582/measurements-and-conversions-guide.html
+//https://www.geeksforgeeks.org/console-clear-method-in-c-sharp/
+//https://www.geeksforgeeks.org/scale-factor/
+//https://stackoverflow.com/questions/52337184/c-getting-user-value-and-resetting-it
+//https://stackoverflow.com/questions/13214081/declare-a-generic-collection
+//https://www.geeksforgeeks.org/c-sharp-delegates/
+//https://learn.microsoft.com/en-us/visualstudio/get-started/csharp/tutorial-wpf?view=vs-2022
+//https://learn.microsoft.com/en-us/dotnet/desktop/wpf/overview/?view=netdesktop-8.0
+//https://stackoverflow.com/questions/4279185/what-is-the-use-of-observablecollection-in-net
+//https://www.c-sharpcorner.com/UploadFile/e06010/observablecollection-in-wpf/
+//https://stackoverflow.com/questions/26196/filtering-collections-in-c-sharp
 
