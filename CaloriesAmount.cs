808b94709b52f9332494e6fdd2e81b9cb542a256
priya <ST10072500@vcconnect.edu.za>
2024-06-27 10:29:15 +0200

    Created Methods for CaloriesAmount

--- CaloriesAmount.cs
+++ CaloriesAmount.cs
@@ -6,7 +6,46 @@ using System.Threading.Tasks;
 
 namespace ST10072500_PROG_6221_Part_3_POE
 {
-    class CaloriesAmount
+    public delegate void CaloriesExceededEventHandler(object sender, EventArgs e);
+
+    public class CaloriesAmount
     {
+        private double calorieLimit;
+        private double calorieTotalAmt;
+
+        public event CaloriesExceededEventHandler CaloriesExceeded;
+
+        private CaloriesAmount() { }
+
+        public CaloriesAmount(double calorieLimit, double calorieTotalAmt)
+        {
+            this.calorieLimit = calorieLimit;
+            this.calorieTotalAmt = calorieTotalAmt;
+        }
+
+        public double CalorieLimit => calorieLimit;
+        public double CalorieTotalAmount => calorieTotalAmt;
+
+        public void Credit(double amount)
+        {
+            if (amount < 0)
+                throw new ArgumentOutOfRangeException(nameof(amount));
+
+            calorieTotalAmt += amount;
+            CheckCalories();
+        }
+
+        protected virtual void OnCaloriesExceeded(EventArgs e)
+        {
+            CaloriesExceeded?.Invoke(this, e);
+        }
+
+        private void CheckCalories()
+        {
+            if (calorieTotalAmt > 300)
+                OnCaloriesExceeded(EventArgs.Empty);
+        }
     }
 }
+
+
