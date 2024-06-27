e67a99c39640b1e7f07759d7788956e532120f7f
priya <ST10072500@vcconnect.edu.za>
2024-06-27 10:25:25 +0200

    Created a CaloriesAmount Class

---
+++ CaloriesAmount.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10072500_PROG_6221_Part_3_POE
+{
+    class CaloriesAmount
+    {
+    }
+}
